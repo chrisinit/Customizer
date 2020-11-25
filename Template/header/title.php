403d76ae9585575e55421adde91282aed0f839fb
chrisinit <chris@turkerview.com>
2020-11-25 09:56:42 -0500

    Adjusted header styling
    More prominant link back to the main home dashboard.

--- Template/header/title.php
+++ Template/header/title.php
@@ -1,8 +1,9 @@
 <h1>
     <?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
     <span class="logo">
-      <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>">
+      <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>" style="color: unset; opacity: unset;">
         <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="<?= $this->task->configModel->get('headerlogo_size', '30') ?>">
+        Home
       </a>
     </span>
     <?php else: ?>
@@ -11,7 +12,7 @@
     </span>    
     <?php endif ?>
 
-    <span class="title">
+    <span class="title" style="font-size: 1rem; margin: 0 1rem;">
         <?php if (! empty($project) && ! empty($task)): ?>
             <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
         <?php else: ?>
